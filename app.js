ee043730a55dd797fc2cccbe3fbcb640c78b90e4
Skeltex <dmtr.afanasyev@gmail.com>
2024-10-31 00:03:25 +0400

    switch theme
    bug with form and link

--- app.js
+++ app.js
@@ -12,3 +12,47 @@ window.onscroll = function() {
 function scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const toggleButton = document.getElementById('theme-toggle');
+const body = document.body;
+
+
+if (localStorage.getItem('theme') === 'dark') {
+    body.classList.add('dark');
+    body.classList.remove('light');
+    toggleButton.textContent = '☀️';
+} else {
+    body.classList.add('light');
+    body.classList.remove('dark');
+    toggleButton.textContent = '🌙';
+}
+
+
+toggleButton.addEventListener('click', () => {
+    body.classList.toggle('dark');
+    body.classList.toggle('light');
+    if (body.classList.contains('dark')) {
+        toggleButton.textContent = '☀️';
+        localStorage.setItem('theme', 'dark');
+    } else {
+        toggleButton.textContent = '🌙';
+        localStorage.setItem('theme', 'light');
+    }
+});
+
