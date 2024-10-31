cd75f2f2d78bb605bc500845041d17f0084b66bf
mickleon <68492322+mickleon@users.noreply.github.com>
2024-10-31 08:32:02 +0400

    added animation for button

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
+const button = document.getElementById("scrollToTopBtn");
+const buttonStyle = button.style;
+let buttonX = 0, buttonY = 0;
+
+function buttonTrans(){
+    buttonStyle.transform = `translate(${buttonX}rem, ${buttonY}rem)`;
+}
+
 // Show button when page is scrolled down
 window.onscroll = function() {
-    const button = document.getElementById("scrollToTopBtn");
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        button.style.display = "block";
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        buttonX = -5
+        buttonTrans()
     } else {
-        button.style.display = "none";
+        buttonX = 0
+        buttonTrans()
     }
 };
+
+// Up when mouse is on button
+function btnUp(x) {
+    buttonY = -0.2
+    buttonTrans()
+}
+// Down when mouse isn't on button
+  function btnDown(x) {
+    buttonY = 0
+    buttonTrans()
+}
 
 // Scroll to top function
 function scrollToTop() {
