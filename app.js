a38d30251458a4dde0c7f2dad99d032334fdeb4a
Dmitry <dmtr.afanasyev@gmail.com>
2024-11-01 23:12:21 +0400

    fixed code style

--- app.js
+++ app.js
@@ -1,42 +1,41 @@
 // Scroll to up button
-
-const button = document.getElementById("scrollToTopBtn");
+const button = document.getElementById('scrollToTopBtn');
 const buttonStyle = button.style;
 let buttonX = 0,
-    buttonY = 0;
+  buttonY = 0;
 
 function buttonTrans() {
-    buttonStyle.transform = `translate(${buttonX}rem, ${buttonY}rem)`;
+  buttonStyle.transform = `translate(${buttonX}rem, ${buttonY}rem)`;
 }
 
 // Show button when page is scrolled down
 
-window.onscroll = function() {
-    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-        buttonX = -5
-        buttonTrans()
-    } else {
-        buttonX = 0
-        buttonTrans()
-    }
+window.onscroll = function () {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    buttonX = -5;
+    buttonTrans();
+  } else {
+    buttonX = 0;
+    buttonTrans();
+  }
 };
 
 // Up when mouse is on button
 
 function btnUp(x) {
-    buttonY = -0.2
-    buttonTrans()
+  buttonY = -0.2;
+  buttonTrans();
 }
 
 // Down when mouse isn't on button
 
 function btnDown(x) {
-    buttonY = 0
-    buttonTrans()
+  buttonY = 0;
+  buttonTrans();
 }
 
 // Scroll to top function
 
 function scrollToTop() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+  window.scrollTo({ top: 0, behavior: 'smooth' });
 }
